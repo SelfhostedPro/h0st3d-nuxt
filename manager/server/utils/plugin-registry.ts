a7759af31bae3a01d289166d3b6ae09fe34a40a6
SelfhostedPro <info@selfhosted.pro>
2024-11-08 12:27:45 -0800

    fixing name in index

--- manager/server/utils/plugin-registry.ts
+++ manager/server/utils/plugin-registry.ts
@@ -63,6 +63,7 @@ export class PluginRegistryManager {
         const plugins: PluginManifest[] = [];
 
         for (const name of index) {
+            console.log(`Reading plugin ${name} from registry ${registry.url + name}.json`)
             const templateInfo = await (await fetch(`${registry.url + name}.json`)).json() as TemplateInfo;
             const pluginInfo = await (await fetch(`${templateInfo.url}/refs/heads/${templateInfo.version || 'main'}/${templateInfo.subdir}/plugin.json?token=none`)).json() as PluginManifest;
             const isValid = PluginManifestSchema.safeParse(pluginInfo);
